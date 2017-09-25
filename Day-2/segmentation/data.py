be3bd51fcb02174a796955941f436ca44466c2df
Ivan Kreso <kreso.ivan@gmail.com>
2017-09-26 00:27:42 +0200

    added clean notebook for semantic segmentation

--- Day-2/segmentation/data.py
+++ Day-2/segmentation/data.py
@@ -3,12 +3,7 @@ import numpy as np
 import pickle
 from os.path import join
 
-# def _shuffle_data(data_x, data_y):
-#   indices = np.arange(data_x.shape[0])
-#   np.random.shuffle(indices)
-#   shuffled_data_x = np.ascontiguousarray(data_x[indices])
-#   shuffled_data_y = np.ascontiguousarray(data_y[indices])
-#   return shuffled_data_x, shuffled_data_y
+
 
 def _shuffle_data(data):
   idx = np.arange(data[0].shape[0])
@@ -16,7 +11,8 @@ def _shuffle_data(data):
   shuffled_data = []
   for d in data:
     if type(d) == np.ndarray:
-      d = np.ascontiguousarray(d[idx])
+      # d = np.ascontiguousarray(d[idx])
+      d = d[idx]
     else:
       d = [d[i] for i in idx]
     shuffled_data.append(d)
@@ -47,7 +43,7 @@ class Dataset():
     self.x = data['rgb']
     self.y = data['labels']
     self.names = data['names']
-    self.x = (self.x.astype(np.float32) - self.mean) / self.std
+    # self.x = (self.x.astype(np.float32) - self.mean) / self.std
 
     self.num_examples = self.x.shape[0]
     self.height = self.x.shape[1]
@@ -67,6 +63,8 @@ class Dataset():
     offset = self.cnt * self.batch_size
     x = self.x[offset:offset+self.batch_size]
     y = self.y[offset:offset+self.batch_size]
+    x = (np.ascontiguousarray(x).astype(np.float32) - self.mean) / self.std
+    y = np.ascontiguousarray(y).astype(np.int32)
     names = self.names[offset:offset+self.batch_size]
     self.cnt += 1
     return x, y, names
